d7183e2fe78a3515eaf221e93969513751433aa4
Rohandeshwal <rohandeshwal19@gmail.com>
2020-06-01 10:29:30 +0530

    cards changes read/less

--- src/app/shared/components/cards/cards.component.ts
+++ src/app/shared/components/cards/cards.component.ts
@@ -8,24 +8,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
- 
+  show = false;
+   isCollapsed = false;
+  
   cardsData= [
     {
       img:"./assets/images/services/Disinfection.jpg",
       name:"Sanitisation",
-      description:"Offices, schools, nursing homes,  churches, day care centres gyms  and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons.",
-      detail:"Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons. Our spray is non-corrosive so it’s safe to use where sensitive electronics are located. A fine mist protects over and under all objects so everything your employees, your customers, members, and participants touch are disinfected and deodorized to clinically clean standards. We strictly follow WHO and ICMR Guidelines to prepare our solution."
+      description:"Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons. Our spray is non-corrosive so it’s safe to use where sensitive electronics are located. A fine mist protects over and under all objects so everything your employees, your customers, members, and participants touch are disinfected and deodorized to clinically clean standards. We strictly follow WHO and ICMR Guidelines to prepare our solution.",
+      visible:false,
+      detail:"Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons. Our spray is non-corrosive so it’s safe to use where sensitive electronics are located. A fine mist protects over and under all objects so everything your employees, your customers, members, and participants touch are disinfected and deodorized to clinically clean standards. We strictly follow WHO and ICMR Guidelines to prepare our solution.",
     },
     {
     img:"./assets/images/services/plumber-1.jpg",
     name:"Plumber",
-    description:"Home plumbing services includes fixing blocks and leakages, work to do with taps or sinks, toilet and sanitary work, bathroom fittings or any other residential plumbing services associated with pipes, pumps or tanks.",
+    description:"Home plumbing services includes fixing blocks and leakages, work to do with taps or sinks, toilet and sanitary work, bathroom fittings or any other residential plumbing services associated with pipes, pumps or tanks.All the plumbers are trained and certified. ",
+    visible:false,
     detail :"Home plumbing services includes fixing blocks and leakages, work to do with taps or sinks, toilet and sanitary work, bathroom fittings or any other residential plumbing services associated with pipes, pumps or tanks.",
   },
   {
     img:"./assets/images/services/electrician.jpg",
     name:"Electrician",
     description:"Doorstep repair and hassle free service for all kind of electrical issue for residential and commercial places by our premium and trained technicians. Our all technicians follow safety measures provided by ICMR. ",
+    visible:false,
     detail:"Doorstep repair and hassle free service for all kind of electrical issue for  residential and commercial places by our premium and trained technicians. Our all technicians follow safety measures provided by ICMR."
   },
   // {
@@ -35,30 +40,33 @@ export class CardsComponent implements OnInit {
   //   detail:""
   // },
   {
-    img:"./assets/images/silver-microwave-oven-on-brown-wooden-kitchen-cabinet-3773573.jpg",
+    img:"./assets/images/services/painting.jpg",
     name:"Painting",
-    description:"Professional painting service includes interior and exterior house painting, commercial painting, pressure washing, drywall repair & more. We will make your house the way you want to be. ",
+    description:"Professional painting service includes interior and exterior house painting, commercial  painting, pressure washing, drywall repair &amp; more. We will make your house the way you want to be. Professionals cover all the furniture and tape to edges to get a smooth and clean work.",    visible:false,
     detail:"Professional painting service includes interior and exterior house painting, commercial  painting, pressure washing, drywall repair &amp; more. We will make your house the way you want to be. Professionals cover all the furniture and tape to edges to get a smooth and clean work."
   },
   
   {
     img:"./assets/images/services/POP- False ceiling.jpg",
     name:"Home Interior ( POP, False Ceiling)",
-    description:"On call service of professional interior workers who are  expert in providing the luxurious look by working with the client’s requirement and fulfilling their dream house.",
+    description:"On call service of professional interior workers who are expert in providing the luxurious look by working with the client’s requirement and fulfilling their dream house. We use branded products Gypsum Board (Borell) and POP (Shakarni) to provide quality product to the client. Channel is used of Jindal and Lafarge. ",
+    visible:false,
     detail:"On call service of professional interior workers who are expert in providing the luxurious look by working with the client’s requirement and fulfilling their dream house. We use branded products Gypsum Board (Borell) and POP (Shakarni) to  provide quality product to the client. Channel is used of Jindal and Lafarge."
   },
  
   {
     img:"./assets/images/services/AC Service.jpg",
     name:"Home Appliance Repair and Servicing",
-    description:"On call service for repair, installation and uninstallation of appliance like AC, Refrigerator, Washing Machine etc. We use latest technology jet pump for cleaning and service.",
+    description:"On call service for repair, installation and uninstallation of appliance like AC, Refrigerator, Washing Machine etc. We use latest technology jet pump for cleaning and servicing of the appliance. 14 days warranty is provided on the repairing.",
+    visible:false,
     detail:"On call service for repair, installation and uninstallation of  appliance like AC, Refrigerator, Washing Machine etc. We use latest technology jet pump for cleaning and servicing of the appliance. 14 days warranty is provided on the repairing."
   },
   {
     img:"./assets/images/steel.jpg",
     name:"Aluminium,Steel & Glasswork",
-    description:"We provide one stop solution for all your aluminium, steel and glass work needs. Toughened glass, fibre sheet, designer railing and balcony can be done with good quality and low prices.",
-    detail:"We provide one stop solution for all your aluminium, steel and glass work needs. Toughened glass, fibre sheet, designer railing and balcony can be done with good quality and low prices."
+   description:"We provide one stop solution for all your aluminium, steel and glass work needs. Toughened glass, fibre sheet, designer railing and balcony can be done with good quality and low prices. ",
+   visible:false,
+   detail:"We provide one stop solution for all your aluminium, steel and glass work needs. Toughened glass, fibre sheet, designer railing and balcony can be done with good quality and low prices."
   }
   
  
